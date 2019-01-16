39944bdabdd7270eb767614332b4c7beeb427f7d
Megha <Megha@10.16.52.163>
2019-01-16 16:16:01 -0500

    initial commit during class

--- src/ca/sheridancollege/week1/softwarefundamentals/StudentArray.java
+++ src/ca/sheridancollege/week1/softwarefundamentals/StudentArray.java
@@ -23,7 +23,7 @@ public class StudentArray
         
         for(int i=0; i<studentList.length; i++)
         {
-            System.out.println(studentList[i].getName());
+           // System.out.println(studentList[i].getName());
         }
     }
     
